54e20b834f954c18321e8846fc638879d2ceb027
GERMAN TOBIAS <49194475@ort>
2024-08-05 12:05:13 -0300

    creado pantalla de inicio

---
+++ Pantalla de inicio/script.js
@@ -0,0 +1,76 @@
+function Zalayan() {
+    alert('El Zalayan fue muy rico, lamentablemente ya lo sacaron. Solo los OG lo recordarán.');
+}
+
+function Milanesa() {
+    alert("La Milanesa es muy rica, grande y viene con muchas papas fritas. Nunca falla. Su valor es de 4000$. Generalmente está los días jueves y viernes.");
+}
+
+function Pancho() {
+    alert("Grandes, suculentos y muy llenadores, algunos los criticarán por su aspecto, pero lo más importante es lo de adentro. Su valor es de 3500$. Generalmente están los días martes.");
+}
+
+function Nestor() {
+    alert("La Nestor no es fabricada en Aieka, pero igual es sumamente sabrosa. Cocinada en Sushi Ko, su valor varía dependiendo del salmón. Si está panizada vale 5500$ y si no 5000$. Además, viene con queso, palta y arroz. Generalmente está los días martes y jueves.");
+}
+
+function Ensalada() {
+    alert("La ensalada es personalizada por el consumidor. Le puedes poner una proteína, 2 salsas, una base de carbohidratos y hasta 5 verduras. Su valor varía de 3500$, 4000$, o 4500$ dependiendo de lo que uno le haya puesto. Se puede comprar todos los días.");
+}
+
+function Empanadas() {
+    alert("Las empanadas generalmente están como menú secundario los días martes y viernes. Vienen 3 unidades y se resalta su rico sabor. Los OG recordarán cuando venían con papas. Su valor es de 3500$.");
+}
+
+function Lajmashin() {
+    alert("Los Lajmashin son muy grandes, vienen 4 unidades y generalmente están los días lunes y/o martes. Los OG recordarán cuando venían con papas fritas. Su valor es de 3500$.");
+}
+
+function Pletzalej() {
+    alert("El Pletzalej es de las cosas más ricas de Aieka, grande con mucho pastrón y papas fritas. Está los días jueves y cuesta 4000$.");
+}
+
+function Nuggets() {
+    alert("Los Nuggets están los días lunes, vienen en mucha cantidad y son muy sabrosos. Presentes los días lunes cuestan 3500$.");
+}
+
+function Albondigas() {
+    alert("Las Albóndigas están los días lunes, empapadas en salsa de tomate, con guarnición de arroz. Son sabrosas. Están los días lunes y cuestan 3500$.");
+}
+
+function Hamburguesa() {
+    alert("A lo largo de la historia fueron cambiando, pero siempre fueron ricas. Generalmente los días miércoles son muy grandes, vienen con papas fritas y cuestan 4000$.");
+}
+
+function PolloTeriyaki() {
+    alert("El Pollo Teriyaki está presente los días martes. La salsa teriyaki tiene mucho sabor y a muchos les encanta. Su valor es de 3500$.");
+}
+
+function PolloChino() {
+    alert("El Pollo Chino fue añadido recientemente. Varía una semana y una semana los días martes con el Pollo Teriyaki. La diferencia es que el Pollo Chino tiene una salsa menos dulce y las piezas de pollo ya vienen panizadas y con la salsa añadida.");
+}
+function Knishes(){
+    alert("Los Knishes, rellenos con papa no se hacen muy presentes, no tiene dia definido, viene 4 unidades y cuestan 3500$")
+
+}
+function ChopSuey(){
+    alert("El Chop suey esta extremadamente infravalorado, sumamente abundante con un viaje mental a China, son muy ricos viene con veruditas y una salsa no muy invasiva. Generalmente los dias Jueves con un valor de 3500$")
+}
+function Salchipapa(){
+    alert("La Salcipapa anteriormente venia en cono y ahora en  bandeja cuestan 3500$ y vienen en mucha cantidad, generalemtne los dias martes.")
+}
+function Shawarma(){
+    alert("El Shawarma es de las cosas con mas sabor de Aieka, recientemente fueron reintroducidos y viene con mucha carne y sabor, su valor es de 4000$ ")
+
+}
+function DaniMaleh(){
+    alert("Daniel Maleh es el rabino de Aieka Yatay, fuentes dicen que estuvo con lios penales por una perimetral pero sin dudas su sonrisa siempre sera recordada y su forma peculiar de poner los tefis.")
+
+}
+function IoniSirota(){
+    alert("Ioni Sirota, es el rabino general y creador de Aieka, esta los dias martes y viernes en Monta, miercoles en tarbut y los Lunes y Jueves en Yatay.")
+
+}
+function ManiPreter(){
+ alert ("Mani Preter arranco en Aieka en 2022, tiene 2 hijos con su corta edad y es hincha de Nueva  Chicago l o encontrar de Lunes a Viernes en Aieka.")
+}
