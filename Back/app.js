a5f07702bb7016e3b8e8a5615198b648bd2775a9
GANDELMAN URIEL <96003020@ort>
2024-08-06 15:07:53 -0300

    .

--- Back/app.js
+++ Back/app.js
@@ -32,6 +32,11 @@ async function obtenerCoordenadas (paisNombre, paises){
 function calcularDistancia (origen, destino){
     return geolib.getDistance(origen, destino) / 1000;
 }
+function calcularDireccion (origen, destino){
+  return geolib.getCompassDirection(origen, destino);
+
+
+}
 
 function generarPaisAleatorio(paises){
     let paisAleatorio = paises[Math.floor(Math.random() * paises.length)];
@@ -42,8 +47,9 @@ async function obtenerDistanciaEntrePaises(paises, paisElegido){
     let origen = await obtenerCoordenadas(paisElegido, paises);
     let destino = await obtenerCoordenadas(paisAleatorio, paises);
     let distancia = calcularDistancia(origen, destino);
+    let direccion = calcularDireccion(origen, destino);
     distancia = Math.round(distancia);
-    return { origen, destino, distancia , paisAleatorio};
+    return { origen, destino, distancia , paisAleatorio , direccion};
 }
 async function main(){
   try {
@@ -51,6 +57,7 @@ async function main(){
       let paisElegido = "Argentina"; // Placeholder
       let resultado = await obtenerDistanciaEntrePaises(paises, paisElegido);
       console.log(`La distancia entre ${paisElegido} y ${resultado.paisAleatorio} es de ${resultado.distancia} km`);
+      console.log(`El pais se encuentra en direccion: ${resultado.direccion}`);
     } catch (error) {
       console.error(error.message);
     }
