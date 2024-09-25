a4ec9c6bcba68c1abd6cfe6a46cf99113ac7da7d
Simiraider <simon.flomenboim@gmail.com>
2024-09-25 11:58:50 -0300

    Update script.js

--- Front/Pantalla Fechas/script.js
+++ Front/Pantalla Fechas/script.js
@@ -22,11 +22,11 @@ const confirmarFecha = () => {
         return;
     }
 
-    if (letraActual != 10) return; 
+    if (letraActual != 10) return;
     const letras = filas[filaActual].querySelectorAll('.letra');
     let fechaIngresada = Array.from(letras).map(letter => letter.textContent).join('');
 
-    if (!listaFechas.includes(fechaIngresada)) {
+    if (listaFechas.indexOf(fechaIngresada) === -1) {
         mensajeFinal = `Esta fecha no está incluida en el diccionario, acuérdate se escribe, XX-XX-XXXX y va de 0 a 9`;
         finalDiv.innerHTML = mensajeFinal;
         return;
@@ -42,7 +42,7 @@ const confirmarFecha = () => {
 
     let sum = 0;
 
-    const compararYAsignarColor = (valorIngresado, valorCorrecto, index, esParteCorrecta) => {
+    const compararYAsignarColor = (valorIngresado, valorCorrecto, index, esParteCorrecta, correctosUsados) => {
         const letter = letras[index];
         letter.classList.add('checked');
         const tecla = document.querySelector(`.key[data-key="${letter.textContent.toLowerCase()}"]`);
@@ -51,7 +51,8 @@ const confirmarFecha = () => {
             letter.style.backgroundColor = 'green';
             if (tecla) tecla.style.backgroundColor = 'green';
             sum++;
-        } else if (esParteCorrecta) {
+            correctosUsados.push(valorIngresado); 
+        } else if (esParteCorrecta && correctosUsados.indexOf(valorIngresado) === -1) {
             letter.style.backgroundColor = 'yellow';
             if (tecla && tecla.style.backgroundColor !== 'green') {
                 tecla.style.backgroundColor = 'yellow';
@@ -62,75 +63,85 @@ const confirmarFecha = () => {
         }
     };
 
-    compararYAsignarColor(diaIngresado[0], diaCorrecto[0], 0, diaCorrecto.includes(diaIngresado[0]));
-    compararYAsignarColor(diaIngresado[1], diaCorrecto[1], 1, diaCorrecto.includes(diaIngresado[1]));
-    compararYAsignarColor(mesIngresado[0], mesCorrecto[0], 3, mesCorrecto.includes(mesIngresado[0]));
-    compararYAsignarColor(mesIngresado[1], mesCorrecto[1], 4, mesCorrecto.includes(mesIngresado[1]));
-    for (let i = 6; i < 10; i++) {
-        compararYAsignarColor(añoIngresado[i - 6], añoCorrecto[i - 6], i, añoCorrecto.includes(añoIngresado[i - 6]));
-    }
+    let correctosUsados = [];
 
-    if (fechaIngresada === '11-09-2001') { // atentado torres gemelas
-        const imagen = document.getElementById('imagen-easter-egg');
+    compararYAsignarColor(diaIngresado[0], diaCorrecto[0], 0, diaCorrecto.indexOf(diaIngresado[0]) !== -1, correctosUsados);
+    compararYAsignarColor(diaIngresado[1], diaCorrecto[1], 1, diaCorrecto.indexOf(diaIngresado[1]) !== -1, correctosUsados);
+    compararYAsignarColor(mesIngresado[0], mesCorrecto[0], 3, mesCorrecto.indexOf(mesIngresado[0]) !== -1, correctosUsados);
+    compararYAsignarColor(mesIngresado[1], mesCorrecto[1], 4, mesCorrecto.indexOf(mesIngresado[1]) !== -1, correctosUsados);
+    compararYAsignarColor(mesIngresado[0], mesCorrecto[0], 6, mesCorrecto.indexOf(añoIngresado[0]) !== -1, correctosUsados);
+    compararYAsignarColor(mesIngresado[1], mesCorrecto[1], 7, mesCorrecto.indexOf(añoIngresado[1]) !== -1, correctosUsados);
+    compararYAsignarColor(mesIngresado[0], mesCorrecto[0], 8, mesCorrecto.indexOf(añoIngresado[0]) !== -1, correctosUsados);
+    compararYAsignarColor(mesIngresado[1], mesCorrecto[1], 9, mesCorrecto.indexOf(añoIngresado[1]) !== -1, correctosUsados);
+    for (let i = 6; i < 10; i++) {
+        compararYAsignarColor(añoIngresado[i - 6], añoCorrecto[i - 6], i, añoCorrecto.indexOf(añoIngresado[i - 6]) !== -1, correctosUsados);
+    }
+    if (fechaIngresada === '11-09-2001') /* atentado torres gemelas */ {
+        let imagen = document.getElementById('imagen-easter-egg');
         imagen.innerHTML = '<img src="descarga(torres-gemelas).png" alt="Imagen" />';
     }
-    if (fechaIngresada === '04-08-0070') { // destrucción del Beit Hamikdash
-       const imagen = document.getElementById('imagen-easter-egg');
-       imagen.innerHTML = '<img src="descarga(beit-hamikdash-caida).png" alt="Imagen" />'
+    if (fechaIngresada === '04-08-0070') /* destrucción del primer beit hamikdash */ {
+       let imagen = document.getElementById('imagen-easter-egg');
+       imagen.innerHTML = '<img src="descarga(beit-hamikdash-caida).png" alt="Imagen" />';
     }
-    if (fechaIngresada === '06-08-1945') { // hiroshima
-        const imagen = document.getElementById('imagen-easter-egg');
-        imagen.innerHTML = '<img src="imagen(hiroshima).png" alt="Imagen" />'
+    if (fechaIngresada === '06-08-1945') /* hiroshima */ {
+        let imagen = document.getElementById('imagen-easter-egg');
+        imagen.innerHTML = '<img src="imagen(hiroshima).png" alt="Imagen" />';
     }
-    if (fechaIngresada === '09-08-1945') { // little boy
-        const imagen = document.getElementById('imagen-easter-egg');
-        imagen.innerHTML = '<img src="descarga(nagasaki).png" alt="Imagen" />'
+    if (fechaIngresada === '09-08-1945') /* nagasaki */ {
+        let imagen = document.getElementById('imagen-easter-egg');
+        imagen.innerHTML = '<img src="descarga(nagasaki).png" alt="Imagen" />';
     }
-    if (fechaIngresada === '23-08-2008') { // cumple de gandel
-        const imagen = document.getElementById('imagen-easter-egg');
-        imagen.innerHTML = '<img src="99615.png" alt="Imagen" />'
+    if (fechaIngresada === '23-08-2008') /* cumple gandel */ {
+        let imagen = document.getElementById('imagen-easter-egg');
+        imagen.innerHTML = '<img src="cumple(gandel).png" alt="Imagen" />';
     }
-    if (fechaIngresada === '23-06-2009') { // cumple de manu
-        const imagen = document.getElementById('imagen-easter-egg');
-        imagen.innerHTML = '<img src="90821.png" alt="Imagen" />'
+    if (fechaIngresada === '23-06-2009') /* cumple manu */ {
+        let imagen = document.getElementById('imagen-easter-egg');
+        imagen.innerHTML = '<img src="cumple(manu).png" alt="Imagen" />';
     }
-    if (fechaIngresada === '22-12-2008') { // cumple de tobi
-        const imagen = document.getElementById('imagen-easter-egg');
-        imagen.innerHTML = '<img src="91075.png" alt="Imagen" />'
+    if (fechaIngresada === '22-12-2008') /* cumple tobi  */ {
+        let imagen = document.getElementById('imagen-easter-egg');
+        imagen.innerHTML = '<img src="cumple(tobi).png" alt="Imagen" />';
     }
-    if (fechaIngresada === '09-01-2009') { // cumple de oneto
-        const imagen = document.getElementById('imagen-easter-egg');
-        imagen.innerHTML = '<img src="96400.png" alt="Imagen" />'
+    if (fechaIngresada === '09-01-2009') /* cumple oneto */ {
+        let imagen = document.getElementById('imagen-easter-egg');
+        imagen.innerHTML = '<img src="cumple(oneto).png" alt="Imagen" />';
     }
-    if (fechaIngresada === '16-04-2009') { // cumple de pipa
-        const imagen = document.getElementById('imagen-easter-egg');
-        imagen.innerHTML = '<img src="106798.png" alt="Imagen" />'
+    if (fechaIngresada === '16-04-2009') /* cumple pipa */ {
+        let imagen = document.getElementById('imagen-easter-egg');
+        imagen.innerHTML = '<img src="cumple(pipa).png" alt="Imagen" />';
     }
-    if (fechaIngresada === '07-02-2023') { // creacion de skibidi toilet
-        const imagen = document.getElementById('imagen-easter-egg');
-        imagen.innerHTML = '<img src="descarga (skibidi-toilet).png" alt="Imagen" />'
+    if (fechaIngresada === '07-02-2023') /* creacion de skibidi toilet */ {
+        let imagen = document.getElementById('imagen-easter-egg');
+        imagen.innerHTML = '<img src="descarga (skibidi-toilet).png" alt="Imagen" />';
     }
-    if (fechaIngresada === '26-09-2008') { // cumple sakito
-        const imagen = document.getElementById('imagen-easter-egg');
-        imagen.innerHTML = '<img src="96912.png" alt="Imagen" />'
-    }        
-    if (fechaIngresada === '01-12-2019') { // creacion de aieka
-        const imagen = document.getElementById('imagen-easter-egg');
-        imagen.innerHTML = '<img src="descarga(aieka).png" alt="Imagen" />'
-    }      
-    if (fechaIngresada === '26-04-1986') { // chernobyl
-        const imagen = document.getElementById('imagen-easter-egg');
-        imagen.innerHTML = '<img src="descarga(chernobyl).png" alt="Imagen" />'
+    if (fechaIngresada === '26-09-2008') /* cumple sakito */ {
+        let imagen = document.getElementById('imagen-easter-egg');
+        imagen.innerHTML = '<img src="cumple(sakito).png" alt="Imagen" />';
     }
-    if (fechaIngresada === '03-08-2008') { // cumple fogo
-        const imagen = document.getElementById('imagen-easter-egg');
-        imagen.innerHTML = '<img src="96879.png" alt="Imagen" />'
-    }  
-    if (fechaIngresada === '01-09-1939') { // invasion de polonia
-        const imagen = document.getElementById('imagen-easter-egg');
-        imagen.innerHTML = '<img src="descarga(invasion-polonia).png" alt="Imagen" />'
-    }   
-    if (filaActual == 6) {          
+    if (fechaIngresada === '01-12-2019') /* creacion de aieka monta*/ {
+        let imagen = document.getElementById('imagen-easter-egg');
+        imagen.innerHTML = '<img src="descarga(aieka).png" alt="Imagen" />';
+    }
+    if (fechaIngresada === '26-04-1986') /* chernobyl */ {
+        let imagen = document.getElementById('imagen-easter-egg');
+        imagen.innerHTML = '<img src="descarga(chernobyl).png" alt="Imagen" />';
+    }
+    if (fechaIngresada === '03-08-2008') /* cumple fogo */ {
+        let imagen = document.getElementById('imagen-easter-egg');
+        imagen.innerHTML = '<img src="cumple(fogo).png" alt="Imagen" />';
+    }
+    if (fechaIngresada === '01-09-1939') /* invasion a polonia */ {
+        let imagen = document.getElementById('imagen-easter-egg');
+        imagen.innerHTML = '<img src="descarga(invasion-polonia).png" alt="Imagen" />';
+    }
+    if (fechaIngresada === '10-05-2009') /* cumple simi */ {
+        let imagen = document.getElementById('imagen-easter-egg');
+        imagen.innerHTML = '<img src="cumple(simi).png" alt="Imagen" />'; 
+    }
+    
+    if (filaActual == 6) {
         mensajeFinal = `Has alcanzado el número máximo de intentos. La fecha correcta era: ${fechaCorrecta}`;
         finalDiv.innerHTML = mensajeFinal;
         juegoTerminado = true;
@@ -154,7 +165,7 @@ document.addEventListener('keydown', e => {
     } else if (e.key === 'Backspace' && letraActual > 0) {
         letraActual--;
         letras[letraActual].textContent = '';
-    } else if (e.key.match(/^[0-9\-]$/) && letraActual < 10) {  
+    } else if (e.key.match(/^[0-9\-]$/) && letraActual < 10) {
         letras[letraActual].textContent = e.key;
         letraActual++;
     }
@@ -180,6 +191,7 @@ teclas.forEach(tecla => {
         }
     });
 });
+
 
 const botonEnter = document.getElementById('enviar');
 botonEnter.addEventListener("click", function() {
