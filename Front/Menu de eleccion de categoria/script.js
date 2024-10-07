4c1732028d0792cf7b492eeb8e6824ece8904fc6
Simiraider <49433670@est.ort.edu.ar>
2024-10-07 11:46:42 -0300

    nigga

--- Front/Menu de eleccion de categoria/script.js
+++ Front/Menu de eleccion de categoria/script.js
@@ -1,16 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("jugar").addEventListener("click", function() {
         const wordleSelect = document.getElementById("Wordle");
-        const deportistasSelect = document.getElementById("Gamedle");
-        const selectedWordleOption = wordleSelect.options[wordleSelect.selectedIndex].value;
-        const selectedDeportistasOption = deportistasSelect.options[deportistasSelect.selectedIndex].value;
-
-        if (selectedWordleOption === "Normal") {
+        const selectedOption = wordleSelect.options[wordleSelect.selectedIndex].value;
+  
+        if (selectedOption === "Normal") {
             window.location.href = "http://127.0.0.1:5500/Front/Pantalla%20Wordle/";
-        } else if (selectedWordleOption === "Fechas") {
+        }
+        else if (selectedOption === "Fechas") {
             window.location.href = "http://127.0.0.1:5500/Front/Pantalla%20Fechas/";
-        } else if (selectedDeportistasOption === "Gamedle") {
-            window.location.href = "http://127.0.0.1:5500/Front/Pantalla%20Gamedle/";
+  
         }
     });
-});
+  });
