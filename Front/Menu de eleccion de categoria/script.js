76936dca120c1ceb483b77b3e74ee32bb0c4fabd
Simiraider <49433670@est.ort.edu.ar>
2024-10-21 10:11:40 -0300

    hice algunaas cosutas

--- Front/Menu de eleccion de categoria/script.js
+++ Front/Menu de eleccion de categoria/script.js
@@ -1,14 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("jugar").addEventListener("click", function() {
+        // Obtener el valor seleccionado del menú "Wordle"
         const wordleSelect = document.getElementById("Wordle");
-        const selectedOption = wordleSelect.options[wordleSelect.selectedIndex].value;
-  
-        if (selectedOption === "Normal") {
+        const Opcionwordle = wordleSelect.options[wordleSelect.selectedIndex].value;
+
+        // Verificar la opción seleccionada en "Wordle"
+        if (Opcionwordle === "Normal") {
             window.location.href = "http://127.0.0.1:5500/Front/Pantalla%20de%20inicio-Wordle/";
         }
-        else if (selectedOption === "Fechas") {
+        else if (Opcionwordle === "Fechas") {
             window.location.href = "http://127.0.0.1:5500/Front/Pantalla%20Fechas/";
-  
+        }
+
+        // Obtener el valor seleccionado del menú "Deportistas"
+        const GamedleSelect = document.getElementById("Deportistas");
+        const OpcionGamedle = GamedleSelect.options[GamedleSelect.selectedIndex].value;
+
+        // Verificar la opción seleccionada en "Deportistas" y redirigir
+        if (OpcionGamedle === "Basket") {
+            window.location.href = "http://127.0.0.1:5500/Back/Gamedle%202/Gamedle/public/basquet/";
+        }
+        else if (OpcionGamedle === "Formula 1") {
+            window.location.href = "http://127.0.0.1:5500/Back/Gamedle%202/Gamedle/public/formula1";
         }
     });
-  });
+});
